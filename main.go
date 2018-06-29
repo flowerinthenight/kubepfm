24acf94a815a56de46d423214832f1a9c124ec50
flowerinthenight <chew.esmero@mobingi.com>
2018-06-29 15:50:34 +0900

    initial

--- main.go
+++ main.go
@@ -1,7 +1,141 @@
 package main
 
-import "log"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"regexp"
+	"strings"
+	"syscall"
+
+	"github.com/fatih/color"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+var (
+	green = color.New(color.FgGreen).SprintFunc()
+	red   = color.New(color.FgRed).SprintFunc()
+
+	targets []string
+
+	cs []*exec.Cmd
+
+	rootCmd = &cobra.Command{
+		Use:   "kubepfm",
+		Short: "simple port-forward wrapper tool for multiple pods",
+		Long:  "Simple port-forward wrapper tool for multiple pods.",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(targets) == 0 {
+				failx("need at least one target")
+			}
+
+			podsList, err := exec.Command("kubectl", "get", "pod").CombinedOutput()
+			if err != nil {
+				failx(err)
+			}
+
+			info("Your pods:")
+			fmt.Printf(string(podsList))
+
+			for _, c := range targets {
+				t := strings.Split(c, ":")
+				if len(t) == 3 {
+					args := []string{
+						"get",
+						"pod",
+						"--no-headers=true",
+						"-o",
+						"custom-columns=:metadata.name",
+					}
+
+					podNames, _ := exec.Command("kubectl", args...).CombinedOutput()
+					re := regexp.MustCompile(t[0] + ".*")
+					targetList := re.FindAllString(string(podNames), -1)
+					if len(targetList) > 0 {
+						addcmd := exec.Command("kubectl", "port-forward", targetList[0], t[1]+":"+t[2])
+						cs = append(cs, addcmd)
+					}
+				} else {
+					fail("invalid target", c)
+				}
+			}
+
+			done := make(chan error)
+
+			// Start all cmds.
+			for _, c := range cs {
+				go func(kcmd *exec.Cmd) {
+					outpipe, err := kcmd.StdoutPipe()
+					if err != nil {
+						failx(err)
+					}
+
+					errpipe, err := kcmd.StderrPipe()
+					if err != nil {
+						failx(err)
+					}
+
+					err = kcmd.Start()
+					if err != nil {
+						failx(err)
+					}
+
+					go func() {
+						outscan := bufio.NewScanner(outpipe)
+						log.Printf("%v|stdout: %v", green(kcmd.Args), outscan)
+					}()
+
+					go func() {
+						errscan := bufio.NewScanner(errpipe)
+						log.Printf("%v|stderr: %v", green(kcmd.Args), errscan)
+					}()
+
+					kcmd.Wait()
+				}(c)
+			}
+
+			<-done
+		},
+	}
+)
+
+func info(v ...interface{}) {
+	m := fmt.Sprintln(v...)
+	log.Printf("%s %s", green("[info]"), m)
+}
+
+func fail(v ...interface{}) {
+	m := fmt.Sprintln(v...)
+	log.Printf("%s %s", red("[error]"), m)
+}
+
+func failx(v ...interface{}) {
+	fail(v...)
+	os.Exit(1)
+}
 
 func main() {
-	log.Printf("hello")
+	go func() {
+		s := make(chan os.Signal)
+		signal.Notify(s, syscall.SIGINT, syscall.SIGTERM)
+		sig := errors.Errorf("%s", <-s)
+		_ = sig
+
+		for _, c := range cs {
+			err := c.Process.Signal(syscall.SIGTERM)
+			if err != nil {
+				info("failed to terminate process, force kill...")
+				_ = c.Process.Signal(syscall.SIGKILL)
+			}
+		}
+
+		os.Exit(0)
+	}()
+
+	rootCmd.Flags().StringSliceVar(&targets, "target", targets, "fmt: pod-name-pattern:local-port:pod-port")
+	rootCmd.Execute()
 }
