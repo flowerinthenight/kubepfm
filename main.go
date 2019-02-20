5ffca789b1339c28b32d07b2f5fbd1d2bfe51672
flowerinthenight <chew.esmero@mobingi.com>
2019-02-20 13:38:31 +0900

    separate function

--- main.go
+++ main.go
@@ -27,128 +27,130 @@ var (
 		Use:   "kubepfm",
 		Short: "simple port-forward wrapper tool for multiple pods",
 		Long:  "Simple port-forward wrapper tool for multiple pods.",
-		Run: func(cmd *cobra.Command, args []string) {
-			if len(targets) == 0 {
-				failx("need at least one target")
+		Run:   Run,
+	}
+)
+
+func Run(cmd *cobra.Command, args []string) {
+	if len(targets) == 0 {
+		failx("need at least one target")
+	}
+
+	cs = make(map[string]*exec.Cmd)
+	podsList, err := exec.Command("kubectl", "get", "pod", "--all-namespaces").CombinedOutput()
+	if err != nil {
+		failx(err)
+	}
+
+	info("Your pods:")
+	fmt.Printf(string(podsList))
+
+	// Range through our input targets.
+	for _, c := range targets {
+		var args []string
+		var name, portPair string
+		t := strings.Split(c, ":")
+		portPair = t[len(t)-2] + ":" + t[len(t)-1]
+		switch len(t) {
+		case 3:
+			name = t[0]
+			args = []string{
+				"get",
+				"pod",
+				"--no-headers=true",
+				"--namespace=default",
+				"-o",
+				"custom-columns=:metadata.name,:metadata.namespace",
+			}
+		default:
+			// Rejoin the names excluding namespace and port pair.
+			name = strings.Join(t[1:len(t)-2], ":")
+			args = []string{
+				"get",
+				"pod",
+				"--no-headers=true",
+				fmt.Sprintf("--namespace=%s", t[0]),
+				"-o",
+				"custom-columns=:metadata.name,:metadata.namespace",
+			}
+		}
+
+		pods, err := exec.Command("kubectl", args...).CombinedOutput()
+		if err != nil {
+			fail(err, string(pods))
+			continue
+		}
+
+		rows := strings.Split(string(pods), "\n")
+		for _, row := range rows {
+			parts := strings.Fields(row)
+			if len(parts) != 2 {
+				continue
 			}
 
-			cs = make(map[string]*exec.Cmd)
-			podsList, err := exec.Command("kubectl", "get", "pod", "--all-namespaces").CombinedOutput()
+			re := regexp.MustCompile(name + ".*")
+			targetList := re.FindAllString(parts[0], -1)
+			if len(targetList) > 0 {
+				addcmd := exec.Command("kubectl", "port-forward", "-n", parts[1], targetList[0], portPair)
+				if _, ok := cs[name]; !ok {
+					cs[name] = addcmd
+				}
+			}
+		}
+	}
+
+	done := make(chan error)
+
+	// Start all cmds.
+	for _, c := range cs {
+		go func(kcmd *exec.Cmd) {
+			outpipe, err := kcmd.StdoutPipe()
 			if err != nil {
 				failx(err)
 			}
 
-			info("Your pods:")
-			fmt.Printf(string(podsList))
-
-			// Range through our input targets.
-			for _, c := range targets {
-				var args []string
-				var name, portPair string
-				t := strings.Split(c, ":")
-				portPair = t[len(t)-2] + ":" + t[len(t)-1]
-				switch len(t) {
-				case 3:
-					name = t[0]
-					args = []string{
-						"get",
-						"pod",
-						"--no-headers=true",
-						"--namespace=default",
-						"-o",
-						"custom-columns=:metadata.name,:metadata.namespace",
-					}
-				default:
-					// Rejoin the names excluding namespace and port pair.
-					name = strings.Join(t[1:len(t)-2], ":")
-					args = []string{
-						"get",
-						"pod",
-						"--no-headers=true",
-						fmt.Sprintf("--namespace=%s", t[0]),
-						"-o",
-						"custom-columns=:metadata.name,:metadata.namespace",
-					}
-				}
-
-				pods, err := exec.Command("kubectl", args...).CombinedOutput()
-				if err != nil {
-					fail(err, string(pods))
-					continue
-				}
-
-				rows := strings.Split(string(pods), "\n")
-				for _, row := range rows {
-					parts := strings.Fields(row)
-					if len(parts) != 2 {
-						continue
-					}
-
-					re := regexp.MustCompile(name + ".*")
-					targetList := re.FindAllString(parts[0], -1)
-					if len(targetList) > 0 {
-						addcmd := exec.Command("kubectl", "port-forward", "-n", parts[1], targetList[0], portPair)
-						if _, ok := cs[name]; !ok {
-							cs[name] = addcmd
-						}
-					}
-				}
+			errpipe, err := kcmd.StderrPipe()
+			if err != nil {
+				failx(err)
 			}
 
-			done := make(chan error)
-
-			// Start all cmds.
-			for _, c := range cs {
-				go func(kcmd *exec.Cmd) {
-					outpipe, err := kcmd.StdoutPipe()
-					if err != nil {
-						failx(err)
-					}
-
-					errpipe, err := kcmd.StderrPipe()
-					if err != nil {
-						failx(err)
-					}
-
-					err = kcmd.Start()
-					if err != nil {
-						failx(err)
-					}
-
-					go func() {
-						outscan := bufio.NewScanner(outpipe)
-						for {
-							chk := outscan.Scan()
-							if !chk {
-								break
-							}
-
-							stxt := outscan.Text()
-							log.Printf("%v|stdout: %v", green(kcmd.Args), stxt)
-						}
-					}()
-
-					go func() {
-						errscan := bufio.NewScanner(errpipe)
-						for {
-							chk := errscan.Scan()
-							if !chk {
-								break
-							}
-
-							stxt := errscan.Text()
-							log.Printf("%v|stderr: %v", green(kcmd.Args), stxt)
-						}
-					}()
-
-					kcmd.Wait()
-				}(c)
+			err = kcmd.Start()
+			if err != nil {
+				failx(err)
 			}
 
-			<-done
-		},
+			go func() {
+				outscan := bufio.NewScanner(outpipe)
+				for {
+					chk := outscan.Scan()
+					if !chk {
+						break
+					}
+
+					stxt := outscan.Text()
+					log.Printf("%v|stdout: %v", green(kcmd.Args), stxt)
+				}
+			}()
+
+			go func() {
+				errscan := bufio.NewScanner(errpipe)
+				for {
+					chk := errscan.Scan()
+					if !chk {
+						break
+					}
+
+					stxt := errscan.Text()
+					log.Printf("%v|stderr: %v", green(kcmd.Args), stxt)
+				}
+			}()
+
+			kcmd.Wait()
+		}(c)
 	}
-)
+
+	<-done
+}
 
 func info(v ...interface{}) {
 	m := fmt.Sprintln(v...)
